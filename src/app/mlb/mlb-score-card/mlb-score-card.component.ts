0d641ab760e3256d4a3528f3b28ce346b9b004e0
MohamadSoufan <mohamad.soufan01@libertymutual.com>
2021-06-24 15:29:10 -0400

    added the router

--- src/app/mlb/mlb-score-card/mlb-score-card.component.ts
+++ src/app/mlb/mlb-score-card/mlb-score-card.component.ts
@@ -1,22 +1,21 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { MlbScore } from '../mlb.service';
 
 @Component({
-  selector: 'app-mlb-score-card',
-  templateUrl: './mlb-score-card.component.html',
-  styleUrls: ['./mlb-score-card.component.scss']
+    selector: 'app-mlb-score-card',
+    templateUrl: './mlb-score-card.component.html',
+    styleUrls: ['./mlb-score-card.component.scss']
 })
 export class MlbScoreCardComponent implements OnInit {
-  @Input() scoreItem!: MlbScore;
+    @Input() scoreItem!: MlbScore;
 
-  constructor() { }
+    constructor(private readonly router: Router) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
-  handleBoxClicked(id: string) {
-    console.log('id from handleBoxClicked', id);
-
-  }
-
+    handleBoxClicked(id: string) {
+        console.log('id from handleBoxClicked', id);
+        this.router.navigateByUrl('/mlb/' + id);
+    }
 }
